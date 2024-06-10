f6ecda55e5566868f245257413392f652581033b
Daniel Frey <dmfrey@gmail.com>
2024-06-10 14:48:58 -0400

    update config and logging

--- secure-app/src/main/java/com/broadcom/springconsulting/ssl_demo/SecureEndpoint.java
+++ secure-app/src/main/java/com/broadcom/springconsulting/ssl_demo/SecureEndpoint.java
@@ -1,5 +1,7 @@
 package com.broadcom.springconsulting.ssl_demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,11 @@ import java.util.Map;
 @RestController
 public class SecureEndpoint {
 
+    private static final Logger log = LoggerFactory.getLogger( SecureEndpoint.class );
+
     @GetMapping( "/secured" )
     public Map<String, String> secured() {
+        log.info( "secured : enter" );
 
         return Map.of( "content", "this is accessible with ssl" );
     }
